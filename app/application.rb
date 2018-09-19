0bf0a3696b986fb3b209e31db83abacc4a6a6a7c
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-09-19 01:10:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,9 @@
- class Application
+class Application
 
-   def call(env)
-     resp = Rack::Response.new
+  def call(env)
+    resp = Rack::Response.new
 
-     t = Time.now
+    t = Time.now
 
     if t.hour >= 12
       resp.write "Good Afternoon!"
@@ -11,22 +11,7 @@
       resp.write "Good Morning!"
     end
 
-     resp.finish
+    resp.finish
    end
 
  end
-
- #     num_1 = Kernel.rand(1..20)
- #     num_2 = Kernel.rand(1..20)
- #     num_3 = Kernel.rand(1..20)
- #
- #     resp.write "#{num_1}\n"
- #     resp.write "#{num_2}\n"
- #     resp.write "#{num_3}\n"
- #
- #     if num_1==num_2 && num_2==num_3
- #       resp.write "You Win"
- #     else
- #       resp.write "You Lose"
- #     end
- #
